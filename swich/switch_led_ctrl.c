ff4e4ac827f3d2dcfcf3feaaf162c8103da1be7a
ronitwilson <ronit.study@gmail.com>
2024-06-19 20:01:46 +0530

    Create switch_led_ctrl.c

---
+++ swich/switch_led_ctrl.c
@@ -0,0 +1,26 @@
+/*
+excersites in https://users.ece.utexas.edu/~valvano/Volume1/E-Book/C8_SwitchLED.htm
+gpio data reg A 0x40004000 
+GPIO PA5 -> 0x40004000 + 80 (which comes from setting 5 bit form [9:2] discribed in page 654
+gpio clock register 0x400FE000 offset 608 set 01
+GPIO A enable -> 0x40004000 ox51c -> set 20
+GPIO A dir -> 0x40004000 offset 0x400 -> set ~20
+*/
+
+#define GPIO_PORTA_BIT_5 (*((volatile unsigned long *)0x40004080))
+#define GPIO_CLOCK (*((volatile unsigned long *)0x400FE608))
+#define GPIO_A_DIR (*((volatile unsigned long *)0x40004512))
+#define GPIO_A_ENABLE (*((volatile unsigned long *)0x40004400))
+
+int main() {
+	GPIO_CLOCK |= 0x00000001;
+	GPIO_A_DIR &= ~0x20;
+	GPIO_A_ENABLE |= 0x20;
+	
+	while(1) {
+		unsigned int currentdata;
+		currentdata = GPIO_PORTA_BIT_5;
+	}		
+	
+	
+}
